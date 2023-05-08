7eb85657ca9b6ffee80d8cf01645447631e306ef
Oswaldo Adrian Osuna <oswaldo.adrian@live.com.mx>
2023-05-07 20:09:52 -0600

    update sql statements order

--- data-warehouse/sql/data-warehouse.sql
+++ data-warehouse/sql/data-warehouse.sql
@@ -31,17 +31,19 @@ CREATE TABLE contacto (
   FOREIGN KEY (cliente_id) REFERENCES cliente(id)
 );
 
-CREATE TABLE produccion (
+CREATE TABLE empleado (
   id INT NOT NULL,
-  lote_produccion VARCHAR(255) NOT NULL,
-  fecha_produccion DATE NOT NULL,
-  no_inspeccion VARCHAR(2) NOT NULL,
-  estado_inspeccion VARCHAR(2) NOT NULL,
-  equipo_id INT NOT NULL,
-  responsable_analisis_id INT NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (equipo_id) REFERENCES equipo(id),
-  FOREIGN KEY (responsable_analisis_id) REFERENCES empleado(id)
+  nombre VARCHAR(255) NOT NULL,
+  puesto VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE proveedor (
+  id INT NOT NULL,
+  nombre VARCHAR(255) NOT NULL,
+  direccion VARCHAR(255) NOT NULL,
+  telefono VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE equipo (
@@ -58,19 +60,17 @@ CREATE TABLE equipo (
   FOREIGN KEY (responsable_equipo_id) REFERENCES empleado(id)
 );
 
-CREATE TABLE empleado (
+CREATE TABLE produccion (
   id INT NOT NULL,
-  nombre VARCHAR(255) NOT NULL,
-  puesto VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE proveedor (
-  id INT NOT NULL,
-  nombre VARCHAR(255) NOT NULL,
-  direccion VARCHAR(255) NOT NULL,
-  telefono VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id)
+  lote_produccion VARCHAR(255) NOT NULL,
+  fecha_produccion DATE NOT NULL,
+  no_inspeccion VARCHAR(2) NOT NULL,
+  estado_inspeccion VARCHAR(2) NOT NULL,
+  equipo_id INT NOT NULL,
+  responsable_analisis_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (equipo_id) REFERENCES equipo(id),
+  FOREIGN KEY (responsable_analisis_id) REFERENCES empleado(id)
 );
 
 CREATE TABLE laboratorio (
